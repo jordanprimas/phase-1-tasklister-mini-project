f58b79cf2e6bcc3650f11dd0438a5c6a1acc4925
Jordan Primas <jorprimas20@gmail.com>
2023-03-19 19:39:00 -0700

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  document.querySelector('form').addEventListener('submit', (event) => {
+    event.preventDefault() 
+    handleToDO(event.target["new-task-description"].value)
+  })
+})
+
+function handleToDO(todo) {
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'x'
+  p.textContent = todo
+  p.appendChild(btn)
+  console.log(p)
+  document.querySelector('#list').appendChild(p)
+}
+
+function handleDelete(event) {
+  event.target.parentNode.remove()
+}
